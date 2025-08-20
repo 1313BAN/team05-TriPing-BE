664d5feeb25272fc64f8a2622e11d8e252eb8131
gyujh630 <rbwjrbxo@ajou.ac.kr>
2025-08-21 01:51:00 +0900

    [Fix] 빌드 에러 해결

--- src/main/java/com/ssafy/enjoytrip/config/AIConfig.java
+++ src/main/java/com/ssafy/enjoytrip/config/AIConfig.java
@@ -1,5 +1,5 @@
 package com.ssafy.enjoytrip.config;
-import org.springframework.ai.chat.client.ChatClient;
+//import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/com/ssafy/enjoytrip/infrastructure/gpt/service/GptServiceImpl.java
+++ src/main/java/com/ssafy/enjoytrip/infrastructure/gpt/service/GptServiceImpl.java
@@ -10,9 +10,9 @@ import com.ssafy.enjoytrip.infrastructure.gpt.dto.RecommendedAttraction;
 import com.ssafy.enjoytrip.util.RedisKeyUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
+//import org.springframework.ai.chat.client.ChatClient;
+//import org.springframework.ai.chat.model.ChatResponse;
+//import org.springframework.ai.chat.prompt.Prompt;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
